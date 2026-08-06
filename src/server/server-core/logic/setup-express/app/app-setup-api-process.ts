f591f70363e0fb544dd6d768c1713828b40dc911
donneko <117331095+donneko@users.noreply.github.com>
2026-08-06 21:48:00 +0900

    fix: propagate API handler promises

--- src/server/server-core/logic/setup-express/app/app-setup-api-process.ts
+++ src/server/server-core/logic/setup-express/app/app-setup-api-process.ts
@@ -14,6 +14,6 @@ export async function setupApiProcess(
     );
 
     context.expressServer.use(apiPrefix, (rep, res) => {
-        deps.apiProcess(rep, res, context);
+        return deps.apiProcess(rep, res, context);
     });
 }
